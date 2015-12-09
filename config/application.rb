d25b6f39c97929802622b5c4fc4f2342a07490c1
musicmunky <musicmunky@gmail.com>
2015-12-09 16:33:02 -0500

    Update with footer changes and initial commit

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module TwandrewsRor
   class Application < Rails::Application
-    config.app_name = "twandrews_ror"
+    config.app_name = "Tim Andrews"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
