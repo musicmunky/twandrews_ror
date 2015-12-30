83c6ac43fd2d27a76d72a0218008b307e44b09e3
musicmunky <musicmunky@gmail.com>
2015-12-30 15:26:40 -0500

    Update to fix order of projects by status

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
+    @projects = Project.all.order(:status)
   end
 
   # GET /projects/1
