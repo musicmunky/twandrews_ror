806317d2d5f17fe1324835835046b3a6eb17772a
musicmunky <musicmunky@gmail.com>
2016-06-03 13:37:07 -0400

    Update with resume entries and better resume css

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ gem 'acts_as_commentable'
 gem 'awesome_print', require:"ap"
 gem 'tinymce-rails'
 
+#gem 'hairtrigger'
+
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/controllers/resume_entries_controller.rb
+++ app/controllers/resume_entries_controller.rb
@@ -1,68 +1,75 @@
 class ResumeEntriesController < ApplicationController
-  before_action :set_resume_entry, only: [:show, :edit, :update, :destroy]
+	before_action :set_resume_entry, only: [:show, :edit, :update, :destroy]
 
-  # GET /resume_entries
-  # GET /resume_entries.json
-  def index
-    @resume_entries = ResumeEntry.all
-  end
+	# GET /resume_entries
+	# GET /resume_entries.json
+	def index
+		@resume_entries = ResumeEntry.all.order("start_date DESC")
+	end
 
-  # GET /resume_entries/1
-  # GET /resume_entries/1.json
-  def show
-  end
+	# GET /resume_entries/1
+	# GET /resume_entries/1.json
+	def show
+	end
 
-  # GET /resume_entries/new
-  def new
-    @resume_entry = ResumeEntry.new
-  end
+	# GET /resume_entries/new
+	def new
+		@resume_entry = ResumeEntry.new
+	end
 
-  # GET /resume_entries/1/edit
-  def edit
-  end
+	# GET /resume_entries/1/edit
+	def edit
+	end
 
-  # POST /resume_entries
-  # POST /resume_entries.json
-  def create
-    @resume_entry = ResumeEntry.new(resume_entry_params)
+	# POST /resume_entries
+	# POST /resume_entries.json
+	def create
+		@resume_entry = ResumeEntry.new(resume_entry_params)
+		#	logger.debug "\n\n\n\n\n\n\nRESUME ENTRY START IS: #{@resume_entry.start_date}\n\n\n\n\n\n\n"
 
-#	logger.debug "\n\n\n\n\n\n\nRESUME ENTRY START IS: #{@resume_entry.start_date}\n\n\n\n\n\n\n"
-#	logger.debug "\n\n\n\n\n\n\nRESUME ENTRY END IS: #{@resume_entry.end_date}\n\n\n\n\n\n\n"
+		if resume_entry_params[:is_current].to_i.equal? 1
+			ResumeEntry.all.update_all(is_current: false)
+		end
 
-    respond_to do |format|
-      if @resume_entry.save
-        format.html { redirect_to resume_url, notice: 'Resume entry was successfully created.' }
-        format.json { render :show, status: :created, location: @resume_entry }
-      else
-        format.html { render :new }
-        format.json { render json: @resume_entry.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+		respond_to do |format|
+			if @resume_entry.save
+				format.html { redirect_to resume_url, notice: 'Resume entry was successfully created.' }
+				format.json { render :show, status: :created, location: @resume_entry }
+			else
+				format.html { render :new }
+				format.json { render json: @resume_entry.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # PATCH/PUT /resume_entries/1
-  # PATCH/PUT /resume_entries/1.json
-  def update
-    respond_to do |format|
-      if @resume_entry.update(resume_entry_params)
-        format.html { redirect_to resume_url, notice: 'Resume entry was successfully updated.' }
-        format.json { render :show, status: :ok, location: @resume_entry }
-      else
-        format.html { render :edit }
-        format.json { render json: @resume_entry.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	# PATCH/PUT /resume_entries/1
+	# PATCH/PUT /resume_entries/1.json
+	def update
 
-  # DELETE /resume_entries/1
-  # DELETE /resume_entries/1.json
-  def destroy
-    @resume_entry.destroy
-    respond_to do |format|
-      format.html { redirect_to resume_entries_url, notice: 'Resume entry was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+		if resume_entry_params[:is_current].to_i.equal? 1
+			ResumeEntry.all.update_all(is_current: false)
+		end
+
+		respond_to do |format|
+			if @resume_entry.update(resume_entry_params)
+				format.html { redirect_to resume_url, notice: 'Resume entry was successfully updated.' }
+				format.json { render :show, status: :ok, location: @resume_entry }
+			else
+				format.html { render :edit }
+				format.json { render json: @resume_entry.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
+	# DELETE /resume_entries/1
+	# DELETE /resume_entries/1.json
+	def destroy
+		@resume_entry.destroy
+		respond_to do |format|
+			format.html { redirect_to resume_entries_url, notice: 'Resume entry was successfully destroyed.' }
+			format.json { head :no_content }
+		end
+	end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -72,6 +79,6 @@ class ResumeEntriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def resume_entry_params
-      params.require(:resume_entry).permit(:position, :company, :company_url, :description, :start_date, :end_date)
+      params.require(:resume_entry).permit(:position, :company, :company_url, :description, :start_date, :end_date, :is_current)
     end
 end
